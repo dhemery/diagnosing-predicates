5186d91c4179069551934f6093a5b480ba57d736
Dale Emery <dale@dhemery.com>
2017-12-15 17:11:05 -0800

    Tests using bare predicates

--- src/main/java/com/dhemery/predicates/PredicateAssert.java
+++ src/main/java/com/dhemery/predicates/PredicateAssert.java
@@ -1,8 +1,46 @@
 package com.dhemery.predicates;
 
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class PredicateAssert {
-    public static void assertThat(String errorMessage, boolean assertion) {
-        if(assertion) return;
+    /**
+     * Asserts that the given value is true
+     *
+     * @param errorMessage the error message for the {@code AssertionError}
+     * @param value        the value to test
+     * @throws AssertionError if the value is false
+     */
+    public static void assertThat(String errorMessage, boolean value) {
+        if (value) return;
         throw new AssertionError(errorMessage);
+    }
+
+    /**
+     * Asserts that the value matches the predicate
+     *
+     * @param errorMessage the error message for the {@code AssertionError}
+     * @param value        the value to test
+     * @param predicate    tests the value
+     * @param <T>          the type of value to test
+     * @throws AssertionError if the value does not match the predicate
+     */
+    public static <T> void assertThat(String errorMessage, T value, Predicate<? super T> predicate) {
+        if (predicate.test(value)) return;
+        throw new AssertionError(errorMessage);
+    }
+
+    /**
+     * Asserts that the value matches the predicate.
+     *
+     * @param value         the value to test
+     * @param predicate     tests the value
+     * @param errorReporter applied to the value to create the error message
+     * @param <T>           the type of value to test
+     * @throws AssertionError if the value does not match the predicate
+     */
+    public static <T> void assertThat(T value, Predicate<? super T> predicate, Function<? super T, String> errorReporter) {
+        if (predicate.test(value)) return;
+        throw new AssertionError(errorReporter.apply(value));
     }
 }
--- src/test/java/com/dhemery/predicates/PredicateAssertTest.java
+++ src/test/java/com/dhemery/predicates/PredicateAssertTest.java
@@ -1,24 +1,119 @@
 package com.dhemery.predicates;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static com.dhemery.predicates.PredicateAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PredicateAssertTest {
     @Test
-    public void  testsBooleanDirectly() {
+    public void
+    testsBooleanDirectly() {
         assertThat("success reason message", true);
 
-        try {
-            assertThat("failing reason message", false);
-        }
-        catch (AssertionError e) {
-            assertEquals("failing reason message", e.getMessage());
-            return;
-        }
+        String errorMessage = "failing reason message";
+        Executable failingAssertion = () -> assertThat(errorMessage, false);
 
-        fail("should have failed");
+        shouldThrow(failingAssertion, errorMessage::equals);
+    }
+
+    @Test
+    public void
+    assertsThatValueMatchesPredicate() {
+        assertThat("success reason message", "non-null", Objects::nonNull);
+
+        String errorMessage = "failing reason message";
+        Executable assertion = () -> assertThat(errorMessage, "non-null", Objects::isNull);
+
+        shouldThrow(assertion, errorMessage::equals);
+    }
+
+    @Test
+    public void
+    appliesErrorReporterToTestedValueToObtainErrorMessage() {
+        String expected = "expected";
+        String actual = "actual";
+        Function<String, String> errorReporter = a -> "<<<" + a + ">>>";
+
+        Executable assertion = () -> assertThat(actual, expected::equals, errorReporter);
+
+        String errorMessage = errorReporter.apply(actual);
+        shouldThrow(assertion, errorMessage::equals);
+    }
+
+    @Disabled("Awaiting DiagnosingPredicate")
+    @Test
+    public void
+    includesDescriptionOfTestedValueInErrorMessage() {
+        String expected = "expected";
+        String actual = "actual";
+
+        String expectedMessage = "identifier\nExpected: \"expected\"\n     but: was \"actual\"";
+
+        Executable assertion = () -> assertThat("identifier", actual, expected::equals);
+
+        shouldThrow(assertion, expectedMessage::startsWith);
+    }
+
+    @Disabled("Awaiting SelfDescribingPredicate")
+    @Test
+    public void
+    descriptionCanBeElided() {
+        String expected = "expected";
+        String actual = "actual";
+
+        String expectedMessage = "\nExpected: \"expected\"\n     but: was \"actual\"";
+
+//        shouldThrow(() -> assertThat(actual, expected::equals), expectedMessage::startsWith);
+    }
+
+    @Disabled("Awaiting DiagnosingPredicate")
+    @Test
+    public void
+    includesMismatchDescription() {
+//        Matcher<String> matcherWithCustomMismatchDescription = new BaseMatcher<String>() {
+//            @Override
+//            public boolean matches(Object item) {
+//                return false;
+//            }
+//
+//            @Override
+//            public void describeTo(Description description) {
+//                description.appendText("Something cool");
+//            }
+//
+//            @Override
+//            public void describeMismatch(Object item, Description mismatchDescription) {
+//                mismatchDescription.appendText("Not cool");
+//            }
+//        };
+//
+//        String expectedMessage = "\nExpected: Something cool\n     but: Not cool";
+//
+//        try {
+//            assertThat("Value", matcherWithCustomMismatchDescription);
+//            fail("should have failed");
+//        } catch (AssertionError e) {
+//            assertEquals(expectedMessage, e.getMessage());
+//        }
+    }
+
+    @Test
+    public void
+    canAssertSubtypes() {
+        Number number = 1;
+        Integer numberAsInteger = number.intValue();
+        assertThat("", numberAsInteger, number::equals);
+    }
+
+    private static void shouldThrow(Executable failingAssertion, Predicate<String> messageTest) {
+        AssertionError thrown = assertThrows(AssertionError.class, failingAssertion);
+        assertTrue(messageTest.test(thrown.getMessage()), "error message");
     }
 }
